e225c9854df38318eb127245ca3901f00bcd3708
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2022-12-12 10:05:49 +0400

    Added checks to catch bad requests

--- index.js
+++ index.js
@@ -8,11 +8,33 @@ const server = http.createServer((req, res) => {
         });
         req.on('end', () => {
             postedData = JSON.parse(body);
-            // Returning the result as a response
+            //console.log(postedData);
+            // Catch clauses
+            if(!postedData.hasOwnProperty('text')){
+                res.writeHead(406, {'Content-type': 'application/json'});
+                res.end('{"error": "Parameter is missing or incorrect"}');
+                return
+            }
+            if(postedData.text.length === 0){
+                res.writeHead(406, {'Content-type': 'application/json'});
+                res.end('{"error": "No text is passed"}');
+                return
+            }
+
+            // split() returns an array of all words, length = # of words
+            // /\s+/ is regex that finds words regardless of # of spaces
+            const wordArray = postedData.text.split(/\s+/);
+
+            // statement to catch the special case of having only spaces
+            if(wordArray[0] === "" && wordArray [1] === ""){
+                res.writeHead(200, {'Content-type': 'application/json'});
+                res.end('{"count": 0}');
+            return
+            }
+
+            // Returning the result after all checks
             res.writeHead(200, {'Content-type': 'application/json'});
-            // split(' ') returns an array of all words, length = # of words
-            res.end(`{"count": ${postedData.text.split(/\s+/).length}}`
-            );
+            res.end(`{"count": ${wordArray.length}}`);
         });
     }
     else{ // To catch bad requests
