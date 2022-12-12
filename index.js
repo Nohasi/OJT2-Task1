f0eec14861139386879888c25a3af13395f528c3
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2022-12-12 09:36:38 +0400

    Added Regex to read string

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const server = http.createServer((req, res) => {
             body += chunk;
         });
         req.on('end', () => {
-
-
+            postedData = JSON.parse(body);
             // Returning the result as a response
-            res.writeHead(200, {'Content-type': 'text/html'});
-            res.end(body);
+            res.writeHead(200, {'Content-type': 'application/json'});
+            // split(' ') returns an array of all words, length = # of words
+            res.end(`{"count": ${postedData.text.split(/\s+/).length}}`
+            );
         });
     }
     else{ // To catch bad requests
@@ -19,7 +20,6 @@ const server = http.createServer((req, res) => {
         res.end('Invalid API Request');
     }
 });
-
 
 server.listen(3000, '127.0.0.1', () => {
     console.log('Listening to requests on port 3000');
